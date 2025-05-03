05547c94b25f893af59aae694d998a9a6021708b
Itzli Molina <itzli2000@msn.com>
2025-05-03 13:09:42 -0600

    refactor(Artifacts): restructure artifact data and improve navigation; remove unused i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    fallback: {
-      en: "es",
-    },
-    routing: {
-      prefixDefaultLocale: true,
-    },
-  },
+  }
 });
