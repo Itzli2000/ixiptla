3f3aaf2db1cde067b7ba10114092946911eb64da
Itzli Molina <itzli2000@msn.com>
2025-05-31 10:51:27 -0600

    feat(artifacts): migrate artifact data to markdown files and update collection logic
    - Replaced JSON artifact data with markdown files for better content management and flexibility.
    - Updated content configuration to use glob loader for markdown files in both English and Spanish.
    - Refactored collection components to accommodate new data structure and improve performance.
    - Removed outdated JSON files to streamline the project structure.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,7 @@
-import { file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const artifactSchema = z.object({
-  id: z.string(),
   title: z.string(),
   culture: z.string(),
   period: z.string(),
@@ -18,18 +17,16 @@ const artifactSchema = z.object({
 });
 
 const artifacts = defineCollection({
-  loader: file('./src/data/artifacts/hum.json'),
+  loader: glob({pattern: "**/*.{md,mdx}", base: './src/content/artifacts/en/'}),
   schema: artifactSchema,
 });
 
 const artefactos = defineCollection({
-  loader: file('./src/data/artefactos/hum.json'),
+  loader: glob({pattern: "**/*.{md,mdx}", base: './src/content/artifacts/es/'}),
   schema: artifactSchema,
 });
 
-export type ArtifactEntry = z.infer<typeof artifactSchema>;
-
 export const collections = {
-  artifacts: artifacts,
-  artefactos: artefactos,
+  artifacts,
+  artefactos,
 } as const;
